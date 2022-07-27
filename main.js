65c95f6c1129e8d1b5603699df6ae315a37a9720
Sam Puddister <sampuddister@gmail.com>
2022-07-27 17:25:32 -0400

    Built the table manager to update the table with new tasks.

---
+++ main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// import taskManager from './taskmanager';
+
+console.log('hi');
+// taskManager();
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0EsaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90by1kby1saXN0LXByb2plY3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IHRhc2tNYW5hZ2VyIGZyb20gJy4vdGFza21hbmFnZXInO1xuXG5jb25zb2xlLmxvZygnaGknKTtcbi8vIHRhc2tNYW5hZ2VyKCk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
